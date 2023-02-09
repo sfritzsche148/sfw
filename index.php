160e1bdb8d27d7a6c3c6b28115f18997f64a4ee1
Sebastian Fritzsche <sebifr58@gmx.de>
2023-02-09 06:35:35 +0100

    Update Server, Design

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <?php
-    $servername = 'localhost:3307';
-    $user = 'root';
-    $pw = '';
-    $db = 'sfw';
+    $servername = 'localhost:3307'; // Servername
+    $user = 'root'; // Serveruser
+    $pw = ''; // Serverpasswort
+    $db = 'sfw'; // Serverdatabase
+
 
     $con = new mysqli($servername, $user, $pw, $db);
 
     if ($con->connect_error) {
         die('No Server Connection'.$con->connect_error);
-    }
+    } 
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $nachricht = $_POST['nachricht'];
 
-    $sql = "INSERT INTO Anfragen (Name, EmailAdresse, Nachricht) VALUES (?, ?, ?)";
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $nachricht = $_POST["nachricht"];
 
-    if ($con->query($sql) === TRUE) {
+    
+    $query = "INSERT INTO Anfragen (Name, EmailAdresse, Nachricht) VALUES ('$name', '$email', '$nachricht')";
+
+
+    if ($con->query($query) === TRUE) {
         echo 'Nachricht wurde gesendet.';
     }else {
         echo 'Die Nachricht konnte nicht verschickt werden' . $con->error;
